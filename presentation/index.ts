5aba13890d25984f9da01b812044b3f62966357e
mouazalkassm <muazkassm99@gmail.com>
2021-04-05 15:48:09 +0300

    added builders and indexes to catch objects

--- presentation/index.ts
+++ presentation/index.ts
@@ -1,18 +1,5 @@
-import { GetAllPayments } from '../domain/usecase/getAllPayments/getAllPayments'
-import { GetAllPaymentRequest } from '../domain/usecase/getAllPayments/getAllPaymentsRequest'
-import { repo } from '../data/index'
-//get all payments
+import PaymentController from '../controllers';
 
-console.log(123456);
+const Payments = new PaymentController().getAllPayments();
 
-
-const payments = (new GetAllPayments(repo)).excute(new GetAllPaymentRequest(
-    {
-        withCost:false,
-        withId:false,
-        withPuropse:false,
-        withWhen:false
-}));
-
-console.log(payments);
-
+console.log(Payments);
